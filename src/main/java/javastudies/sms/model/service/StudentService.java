891fec33995d8cbec2ddaedf125f0a76deab2f40
leonardorafaelli <leonardorafaelli@gmail.com>
2024-04-26 00:40:59 +0200

    feat: adds student repository and service

---
+++ src/main/java/javastudies/sms/model/service/StudentService.java
@@ -0,0 +1,7 @@
+package javastudies.sms.model.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface StudentService {
+}
--- src/main/java/javastudies/sms/repository/StudentRepository.java
+++ src/main/java/javastudies/sms/repository/StudentRepository.java
@@ -2,8 +2,8 @@ package javastudies.sms.repository;
 
 import javastudies.sms.model.entities.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+
 public interface StudentRepository extends JpaRepository<Student, Short> {
+
 }
