b7b50976d10ea2d324d26c1002b185dfffcc6873
leonardorafaelli <leonardorafaelli@gmail.com>
2024-04-26 00:09:56 +0200

    feat: change folder structure and adds repository

---
+++ src/main/java/javastudies/sms/model/entities/Student.java
@@ -0,0 +1,24 @@
+package javastudies.sms.model.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="student")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Short id;
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "email")
+    private String email;
+}
