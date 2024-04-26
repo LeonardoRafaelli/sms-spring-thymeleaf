c9cf2eeadcf15a3640404293dd2e9336a668516f
leonardorafaelli <leonardorafaelli@gmail.com>
2024-04-26 20:14:47 +0200

    feat: adds post to new student through thymeleaf form

--- src/main/java/javastudies/sms/SmsApplication.java
+++ src/main/java/javastudies/sms/SmsApplication.java
@@ -1,13 +1,28 @@
 package javastudies.sms;
 
+import javastudies.sms.model.entities.Student;
+import javastudies.sms.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SmsApplication {
+public class SmsApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SmsApplication.class, args);
 	}
 
+
+	@Autowired
+	private StudentRepository studentRepository;
+
+	// Executes when the application is runned
+	@Override
+	public void run(String... args) throws Exception {
+		Student student = new Student(null, "Leonardo", "Rafaelli", "leonardorafaelli@gmail.com");
+		studentRepository.save(student);
+
+	}
 }
--- src/main/java/javastudies/sms/controller/StudentController.java
+++ src/main/java/javastudies/sms/controller/StudentController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,10 +20,21 @@ public class StudentController {
     @GetMapping("/students")
     public String listStudents(Model model) {
         List<Student> studentList = studentService.getAllStudents();
-        System.out.println("Student LIST: " + studentList);
         model.addAttribute("students", studentList);
         return "students";
     };
 
+    @GetMapping("/students/new")
+    public String createStudentForm(Model model){
+        Student student = new Student();
+        model.addAttribute("student", student);
+        return "create_student";
+    };
+
+    @PostMapping("/students")
+    public String saveStudent(@ModelAttribute("student") Student student) {
+        studentService.saveStudent(student);
+        return "redirect:/students";
+    }
 
 }
